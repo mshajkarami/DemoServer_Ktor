0e61f786e7c586ec8ca305a9ae08c5b853784268
mshajkarami <mshajkarami.dev@gmail.com>
2025-06-23 10:14:48 +0330

    finish First Project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,28 @@
 plugins {
+    application
     kotlin("jvm") version "2.0.0"
 }
 
-group = "org.example"
+group = "ir.hajkarami"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+}
+
+val ktorVersion = "2.3.10"
+val logbackVersion = "1.4.14"
+
 dependencies {
     testImplementation(kotlin("test"))
+    implementation(kotlin("stdlib"))
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
 }
 
 tasks.test {
---
+++ src/main/kotlin/ir/hajkarami/Application.kt
@@ -0,0 +1,17 @@
+package ir.hajkarami
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+
+fun main (args : Array<String>) : Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module() {
+    routing {
+        get("/"){
+            call.respondText("HELLO WORLD!")
+        }
+    }
+
+}
