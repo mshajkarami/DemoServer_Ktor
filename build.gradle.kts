b22ed9d699204d4e6142a4605a853a05228227a0
mshajkarami <mshajkarami.dev@gmail.com>
2025-06-24 11:42:55 +0330

    Add Jwt Config

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,10 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
     implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-auth-jvm:2.3.7")
+    implementation("io.ktor:ktor-server-auth-jwt-jvm:2.3.7")
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
+
 }
 
 tasks.test {
--- src/main/kotlin/ir/hajkarami/Application.kt
+++ src/main/kotlin/ir/hajkarami/Application.kt
@@ -1,16 +1,49 @@
 package ir.hajkarami
 
+import com.auth0.jwk.JwkProviderBuilder
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.net.URL
 
 
-fun main (args : Array<String>) : Unit = io.ktor.server.netty.EngineMain.main(args)
+fun main() {
+    embeddedServer(Netty, port = 8081) {
+        module()
+    }.start(wait = true)
+}
 
 fun Application.module() {
+    install(Authentication) {
+        jwt("auth-jwt") {
+            realm = "iem-dev"
+            verifier(
+                JwkProviderBuilder(URL("http://localhost:8080/realms/iem-dev/protocol/openid-connect/certs"))
+                    .build()
+            )
+            validate { credential ->
+                if (credential.payload.getClaim("preferred_username").asString() != "")
+                    JWTPrincipal(credential.payload)
+                else null
+
+            }
+        }
+    }
     routing {
-        get("/"){
+        get("/") {
             call.respondText("HELLO WORLD!")
+        }
+        authenticate("auth-jwt") {
+            get("/secure") {
+                val principal = call.principal<JWTPrincipal>()
+                val username = principal?.payload?.getClaim("preferred_username")?.asString()
+                call.respondText("Hello $username! You are authenticated.")
+            }
         }
     }
 
